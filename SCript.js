8102890d2fc2cd862cd2375f8326b8abe35496a0
techiemow <mowriyaabk@gmail.com>
2024-10-22 11:40:47 +0530

    To read and write file asynchronously

--- SCript.js
+++ SCript.js
@@ -25,3 +25,22 @@ console.log(textIn)
 
 let content = `Data read from input.txt: ${textIn}. \nDate created ${new Date().toDateString()}`
 fs.writeFileSync('./Files/output.txt', content);
+
+/*LECTURE 7: CODE EXAMPLE**************
+READING & WRITING TO FILE ASYNCHRONOUSLY
+*************************************** */
+fs.readFile('./Files/start.txt', 'utf-8', (error1, data1) => {
+    console.log(data1)
+    fs.readFile(`./Files/${data1}.txt`, 'utf-8', (error2, data2) => {
+        console.log(data2);
+        fs.readFile('./Files/append.txt', 'utf-8', (error3, data3) => {
+            console.log(data3);
+            fs.writeFile('./Files/output.txt', `${data2}\n\n${data3}\n\nDate created ${new Date()}`, () => {
+                console.log('File writen successfully');
+            });
+        })
+    })
+})
+
+
+console.log('Reading file....');
